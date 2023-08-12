de65b087b7774a5365375f90141177ce012960e1
Dmitfree <dmitriykhazgeriev@gmail.com>
2023-08-12 22:40:18 +0300

    Тесты почему-то не работаю. Нужно разобраться.

--- MovieQuizTests/MovieQuizTests.swift
+++ MovieQuizTests/MovieQuizTests.swift
@@ -7,7 +7,10 @@
 
 import XCTest
 
-struct ArithmeticOperations {
+//MARK: - синхронные тесты
+
+/*struct ArithmeticOperations {
+    
     func addition(num1: Int, num2: Int) -> Int {
         return num1 + num2
     }
@@ -35,5 +38,49 @@ final class MovieQuizTests: XCTestCase {
         
         /// Then
         XCTAssertEqual(result, 3)
+    }
+}
+*/
+//MARK: - асинхронные тесты
+
+struct ArithmeticOperationsAsinch {
+    
+    func addition(num1: Int, num2: Int, handler: @escaping (Int) -> Void) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                handler(num1 + num2)
+            }
+        }
+        
+        func subtraction(num1: Int, num2: Int, handler: @escaping (Int) -> Void) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                handler(num1 - num2)
+            }
+        }
+        
+        func multiplication(num1: Int, num2: Int, handler: @escaping (Int) -> Void) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                handler(num1 * num2)
+            }
+        }
+    }
+
+final class MovieQuizAsinchTests: XCTestCase {
+    
+    func testAddition() throws {
+        // Given
+        let arithmeticOperations = ArithmeticOperationsAsinch()
+        let num1 = 1
+        let num2 = 2
+        
+        // When
+        let expectation = expectation(description: "Addition function expectation")
+       
+       arithmeticOperations.addition(num1: num1, num2: num2) { result in
+            // Then
+            XCTAssertEqual(result, 3)
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 2)
     }
 }
